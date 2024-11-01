5201e1eaf3529fe9503556e0e9bc31518b9107c1
Eldorbek94 <eldorbtc@gmail.com>
2024-11-01 10:19:33 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("CHeck prime number!");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number==1) Console.WriteLine("TUB SON EMAS");
+if(number==2) Console.WriteLine("TUB SON");
+if(number==3) Console.WriteLine("TUB SON");
+int res = -1,res1=-1;
+if(number>3) res = (number+1)%6;
+if(res>0) res1 = (number-1)%6;
+if(res1>0) Console.WriteLine($"TUB SON EMAS");
+if(res1==0)Console.WriteLine($"TUB SON");
+if (res==0) Console.WriteLine($"TUB SON");
+if(number<0) Console.WriteLine("Manfiy son kiritdiz");
